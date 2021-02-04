4d96e768abbf9afd894a24a71636bde2ca1b52d3
AdahMilly <oamildred@gmail.com>
2021-02-04 16:08:18 +0300

    add modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,10 +1,22 @@
 import React from "react";
 import "./Modal.css"
+import Backdrop from "../Backdrop/Backdrop"
+
+import Aux from "../../../hoc/Aux"
 
 const modal = (props) => (
-    <div className="Modal">
+    <Aux>
+        <Backdrop show={props.show} clicked={props.modalClosed}/>
+        <div 
+        className="Modal"
+        style ={{
+        transform:props.show ? "translate(0)" :"translate(-100vh)",
+        opacity:props.show ? "1" : "0"
+        }}>
         {props.children}
-    </div>
+        </div>
+    </Aux>
+
 );
 
 export default modal
